67f2ed26cce016fa964d44016ab0a3146ca22f1e
Tom Longhurst <thomhurst@gmail.com>
2022-01-19 12:49:44 +0000

    Adding project to reproduce issue

---
+++ FailingNUnitTestProject/UnitTest1.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FailingNUnitTestProject;
+
+public enum MyEnum
+{
+    Enum1,
+    Enum2,
+    Enum3,
+    Enum4,
+}
+
+[TestFixtureSource(nameof(GetAllMyEnums))]
+public abstract class BaseClass
+{
+    private static readonly IEnumerable<MyEnum> GetAllMyEnums = new[] { MyEnum.Enum1, MyEnum.Enum2, MyEnum.Enum3, MyEnum.Enum4};
+    private readonly MyEnum _myEnum;
+
+    protected BaseClass(MyEnum myEnum)
+    {
+        _myEnum = myEnum;
+    }
+}
+
+public class MyClass : BaseClass
+{
+    public MyClass(MyEnum myEnum) : base(myEnum)
+    {
+    }
+
+    [Test]
+    public async Task MyTest()
+    {
+        await Task.CompletedTask;
+    }
+    
+    [Test]
+    public async Task MyTest2()
+    {
+        await Task.CompletedTask;
+    }
+}
